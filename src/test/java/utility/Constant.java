ac5f3dcdf76661ec69d3b5c40390fefc47aa6b62
nicolasrioseco <37633431+nicolasrioseco@users.noreply.github.com>
2018-05-17 10:26:47 -0300

    ok
    ok

--- src/test/java/utility/Constant.java
+++ src/test/java/utility/Constant.java
@@ -2,6 +2,6 @@ package utility;
 
 public class Constant {
 
-    public static final String Path_TestData = "C:\\Program Files (x86)\\Jenkins\\workspace\\3-CuoreAuto_EjecutarTest\\src\\test\\resources\\testData\\";
+    public static final String Path_TestData = "C:\\";
     public static final String File_TestData = "TestData.xlsx";
 }
