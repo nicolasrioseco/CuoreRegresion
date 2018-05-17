e3a2055b469bcb0ec3f0d59e5a00751b73c8a58f
nicolasrioseco <37633431+nicolasrioseco@users.noreply.github.com>
2018-05-17 17:12:07 -0300

    ok
    ok

--- src/test/java/utility/Constant.java
+++ src/test/java/utility/Constant.java
@@ -2,6 +2,6 @@ package utility;
 
 public class Constant {
 
-    public static final String Path_TestData = "C:\\";
+    public static final String Path_TestData = "C:\\CuoreData\\";
     public static final String File_TestData = "TestData.xlsx";
 }
